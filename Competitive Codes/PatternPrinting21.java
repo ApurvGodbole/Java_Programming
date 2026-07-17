c94a17fa9e96d3a8167f439467f6c0d36125f3c1
Apurv Godbole <apurvgodbole.work@gmail.com>
2026-07-17 09:20:45 +0530

    Implement pattern printing based on user input
    The program prints a pattern of '*' and '#' based on user input for rows and columns. It validates that the number of rows and columns are equal before displaying the pattern.

---
+++ Competitive Codes/PatternPrinting21.java
@@ -0,0 +1,62 @@
+/*
+    INPUT :    iRow : 4    iCol : 4
+
+    OUTPUT :        *   #   #   #
+                    *   *   #   #
+                    *   *   *   #
+                    *   *   *   * 
+
+*/
+
+import java.util.*;
+
+class Print
+{
+    public void Display(int iRow , int iCol)
+    {
+        int i = 0 , j = 0 ;
+
+        if ( iRow != iCol )
+        {
+            System.out.println("Invalid Input ");
+            System.out.println("Number of Rows and Columns should be same ");
+            return ;
+        }
+
+        for(i = 1 ; i <= iRow ; i++)
+        {
+            for(j = 1 ; j <= iCol ; j++)
+            {
+                if (i == j || i>j )
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("#\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class PS26a
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0 , iValue2 = 0;
+
+        System.out.println("Enter Number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter Number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        Print pobj = new Print();
+
+        pobj.Display(iValue1 , iValue2);
+
+    }
+}
